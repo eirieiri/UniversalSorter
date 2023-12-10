afa45b91f494f9e7b089899034de58c70bc35359
eirieiri <klaudia77@live.de>
2023-12-11 00:44:09 +0100

    adding a mergesort

--- src/startSorter.js
+++ src/startSorter.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const buttonContainer = document.getElementById("buttons");
     const textContainer = document.getElementById("text");
 
-    //
     const savedInputs = [];
 
     document.addEventListener("click", function () {
+        //Start manual button is pressed
         if (event.target.id === "start-manual") {
 
             saveInputs(savedInputs);
@@ -23,9 +23,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
             createChoicePage();
         }
+
+        //If the sorter-button is pressed  
+        if (event.target.id === "sorter-button") {
+            buttonContainer.innerHTML = "";
+            inputContainer.innerHTML = "";
+            textContainer.innerHTML = "";
+
+            //start sorter 
+            startSorter(saveInputs);
+        }
     });
 });
 
+/**
+ * saves inputs into an empty array 
+ * @param {Object} array 
+ */
 function saveInputs(array) {
     const inputContainer = document.getElementById("input");
     const options = inputContainer.querySelectorAll(".option");
@@ -37,12 +51,11 @@ function saveInputs(array) {
         if (nameInput.value.trim() !== "" || linkInput.value.trim() !== "") {
             const inputObject = {
                 name: nameInput.value,
-                link: linkInput.value,
+                link: processYoutubelink(linkInput.value),
             };
 
             array.push(inputObject);
         }
-
     });
 }
 
@@ -93,4 +106,65 @@ function createChoicePage() {
     //append the buttons 
     buttonContainer.appendChild(sorterButton);
     buttonContainer.appendChild(bracketButton)
+}
+
+function processYoutubelink(url) {
+    var regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    var match = url.match(regex);
+
+    if (match) {
+        return "https://www.youtube.com/embed/"+match[1];
+    } else {
+        return url;
+    }
+}
+
+//
+function startSorter(array) {
+    //find the different containers 
+    let option_1Container = document.getElementById("opt-1")
+    let option_2Container = document.getElementById("opt-2")
+    
+    mergeSort(array, 1)
+}
+
+//merge function
+function merge(left, right, battleNumber) {
+    let textContainer = document.getElementById("text")
+    let text = document.createElement("h2");
+    h2 = `Battle N.${battleNumber}`;
+
+    battleNumber++
+
+    let arr = [];
+
+    while (left.length && right.left) {
+        if (chooseCharacter() === 0) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+    return [ ...arr, ...left, ...right ]
+}
+
+//mergeSort function 
+function mergeSort(array, battleNumber) {
+
+    let half = array.length / 2;
+
+    if (array.length < 2) {
+        return array
+    }
+
+    const left = array.splice(0, half);
+    return merge(mergeSort(left, battleNumber), mergeSort(array, battleNumber), battleNumber)
+}
+
+//based of what button is pressed returns 1 or 0 
+function chooseCharacter() {
+    let option_1Container = document.getElementById("opt-1")
+    let option_2Container = document.getElementById("opt-2")
+    
+    return 0; 
 }
